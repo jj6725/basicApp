80c52610e9cfb44f66a68bd9bc6e324f3950b43d
jj6725 <mengdaq@gmail.com>
2017-02-18 23:55:10 -0600

    adding radio logic

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory ,IndexRoute } from 'react-router'
-import App from './App';
+import App from './StudentForm';
 import './index.css';
 import NoMatch from './NoMatch.jsx'
 
